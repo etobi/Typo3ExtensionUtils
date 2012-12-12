3f59dba28f3974f6cce630086f6c682ad9736a9c
Tobias Liebig <tobias.liebig@typo3.org>
2012-12-12 11:30:54 +0100

    [TASK] improve createPhar

--- bin/createPhar.php
+++ bin/createPhar.php
@@ -1,15 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-$phar = new Phar(__DIR__ . '/../bin/extension.phar', 0, 'extension.phar');
-$phar->buildFromDirectory(
-	__DIR__ . '/..',
-	'/(bin|lib)\/.*\.php/'
-);
-$phar->setStub(
-	'#!/usr/bin/env php
+// make sure it doesn't exist
+$pharFilepath = __DIR__ . '/../bin/extension.phar';
+@unlink($pharFilepath);
+try {
+	$phar = new Phar($pharFilepath, 0, 'extension.phar');
+	$phar->buildFromDirectory(
+		__DIR__ . '/..',
+		'/(bin|lib)\/.*\.php/'
+	);
+	$phar->setStub(
+		'#!/usr/bin/env php
 <?php
 Phar::mapPhar("extension.phar");
 require "phar://extension.phar/bin/extension.php";
 __HALT_COMPILER();
 ?>');
+} catch (Exception $e) {
+    echo 'Could not create and/or modify phar:', $e;
+}
