650b12d6191110ed561f78a09371b52c1bc3a2b2
Christian Zenker <christian.zenker@599media.de>
2013-07-06 23:53:51 +0200

    [TASK] clean up Ter/LoginCommand and add some documentation

--- lib/etobi/extensionUtils/Command/Ter/LoginCommand.php
+++ lib/etobi/extensionUtils/Command/Ter/LoginCommand.php
@@ -26,8 +26,20 @@ class LoginCommand extends AbstractAuthenticatedTerCommand
             ->setName('ter:login-test')
             ->setDefinition(array())
             ->setDescription('Checks if a username and password are valid credentials for TYPO3 SOAP API')
-            //@TODO: longer help text
-//            ->setHelp()
+            ->setHelp(<<<EOT
+Check if your given username and password are valid credentials for typo3.org.
+
+Some commands form the ter:* namespace require a valid login on typo3.org.
+This command checks if your given credentials are valid.
+
+Example
+=======
+
+Interactively ask for your username and password and check if they are valid on typo3.org
+
+  t3xutils ter:login-test
+EOT
+)
         ;
         $this->configureSoapOptions();
         $this->configureCredentialOptions();
@@ -38,6 +50,7 @@ class LoginCommand extends AbstractAuthenticatedTerCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+	    /** @var \etobi\extensionUtils\T3oSoap\LoginRequest $loginRequest */
         $loginRequest = $this->getRequestObject('\\etobi\\extensionUtils\\T3oSoap\\LoginRequest');
         $result = $loginRequest->checkCredentials();
 
